d200026a6dcdf910fbfe87ed77a82fd223048643
Theodagan <theo@oai-lab.com>
2026-05-17 00:19:56 +0200

    feat: enhance token management page with improved navbar and user context

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -23,6 +23,7 @@ export const routes: Routes = [
   },
   {
     path: 'home',
+    pathMatch: 'full',
     canActivate: [authGuard],
     loadComponent: () => import('./pages/home-page/home-page').then((module) => module.HomePage),
   },
